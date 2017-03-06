b1a1ae1a2d3ca5dce955edfa59c4882de69a5dae
Françoise <goujard.francoise@laposte.net>
2017-03-06 12:12:26 +0100

    Récupère les nom et id des datasets pour chaque villes

--- src/app/Data.java
+++ src/app/Data.java
@@ -5,6 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class Data {
 	public Ville v1;
@@ -18,13 +25,37 @@ public class Data {
 	}
 
 	public void getData() throws IOException {
-		String urlV1 = v1.adress + "api/datasets/1.0/search/?q=" + theme.name;
-		String urlV2 = v1.adress + "api/datasets/1.0/search/?q=" + theme.name;
+		HashMap<String, String> datasetsV1 = rechercherDatasets(v1);
+		HashMap<String, String> datasetsV2 = rechercherDatasets(v2);
 		
-		String contentV1 = getText(urlV1);
-		String contentV2 = getText(urlV2);
+		System.out.println(v1.name);
+		for (Entry<String, String> dataset : datasetsV1.entrySet()) {
+			System.out.println(dataset.getKey() +" : "+ dataset.getValue());
+		}
+		
+		System.out.println(v2.name);
+		for (Entry<String, String> dataset : datasetsV2.entrySet()) {
+			System.out.println(dataset.getKey() +" : "+ dataset.getValue());
+		}
 	}
 
+	public HashMap<String, String> rechercherDatasets(Ville ville) throws IOException {
+		String url = ville.adress + "api/datasets/1.0/search/?q=" + theme.name;
+		String content = getText(url);
+		
+		JsonObject json = new JsonParser().parse(content).getAsJsonObject();
+		JsonArray datasets = json.get("datasets").getAsJsonArray();
+		
+		HashMap<String, String> hashmap = new HashMap<String, String>();
+		for (JsonElement e : datasets) {
+			JsonObject obj1 = e.getAsJsonObject();
+			JsonObject obj2 = obj1.get("metas").getAsJsonObject();
+			hashmap.put(obj1.get("datasetid").getAsString(), obj2.get("title").getAsString());
+		}
+		
+		return hashmap;
+	}
+	
 	public static String getText(String url) throws IOException {
         URL website = new URL(url);
         URLConnection connection = website.openConnection();
@@ -42,7 +73,7 @@ public class Data {
     }
 
     public static void main(String[] args) throws IOException {
-        String content = Data.getText("https://data.rennesmetropole.fr/api/datasets/1.0/search/?q=annuel");
-        System.out.println(content);
+    	Data d = new Data(new Ville("Paris"), new Ville("Rennes"), new Theme("naissances"));
+    	d.getData();
     }
 }
